7a8b7c59c3f6a68a62a60671a970b92f4425e5a9
Paulina-Pereyra <paulinapereyrasantana@gmail.com>
2024-07-04 13:13:51 +0000

    proyecto generador de nombres terminado

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-  // arrays 
-  let pronoun = ['the', 'our', 'his'];
-  let adj = ['great', 'big', 'amazing' ];
-  let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
-  
+let pronoun = ['the', 'our', 'his'];
+let adj = ['great', 'big', 'amazing'];
+let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
 
-  for(let i = 0; i < pronoun.length; i++) {
-    for(let j = 0; j < adj.length; j++) {
-      for(let k = 0; k < noun.length; k++) {
-        return(pronoun[i] + adj[j] + noun[k]+'.com'); 
-      }
+let domainNames = [];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      domainNames.push(pronoun[i] + adj[j] + noun[k] + '.com');
     }
   }
+}
+
+console.log(domainNames);
